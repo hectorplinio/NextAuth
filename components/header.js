c999c0e70e4260b3329756e8468d6bd2be19a38e
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2022-01-09 20:20:31 +0100

    Branch 2 finish

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
+import { data } from "autoprefixer"
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
@@ -8,6 +9,8 @@ import styles from "./header.module.css"
 export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
+  console.log(session)
+
 
   return (
     <header>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -112,7 +112,7 @@ export default NextAuth({
   // You can set the theme to 'light', 'dark' or use 'auto' to default to the
   // whatever prefers-color-scheme is set to in the browser. Default is 'auto'
   theme: {
-    colorScheme: "light",
+    colorScheme: "dark",
   },
 
   // Enable debug messages in the console if you are having problems
